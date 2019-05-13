9e8398252419d7117b87719726c6f7c289156595
davidfernandocastro <osdavidcastrofernandez@gmail.com>
2019-05-13 17:02:11 -0400

    se realizo el ejercicio

--- Hello.py
+++ Hello.py
@@ -1,2 +1,35 @@
-#Hola que hace
-print("hola que tal")
+#creamos un archivo nuevo
+#guardamos en la carpeta del repositorio
+#con la extension .py
+#uso de numero aleatorios
+'''
+#importamos la libreria radiant
+from random import randint 
+aleatorio=randint(0,20)    #creamos una variante aleatorio
+print(aleatorio)           #y generamos un numero aleatorio entre 0 y 20
+#imprimimos el campo aleatorio
+'''
+#ejercicio 
+#escribir una funcion sorteo() que reciba una lista de participantes
+#y eleguir a uno de los participante aleatoriamente y retotno
+#esa persona elegida 
+#desafio: no volver a retornar una persona ya sorteado
+from random import randint
+listaparticipanter=["david","mauro","willy","oscar","rosa","luis"]
+numeroparticipantes=len((listaparticipanter)- 1)
+def sorteo(lista1):
+    aleatorio=randint(0,numeroparticipantes)
+    ganador=listaparticipanter[aleatorio]
+    return ganador
+        
+print(gano)
+listaganadores=[]
+gano=sorteo(listaparticipanter)
+listaganadores.append(gano)
+print(listaganadores)
+
+
+
+  
+
+
